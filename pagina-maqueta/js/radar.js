51802e2e1e5ed2672fc44e4a6514cd1f0e490dca
Denisse <daortega@uc.cl>
2018-04-05 13:40:59 -0400

    radar chart y modal icvu

--- pagina-maqueta/js/radar.js
+++ pagina-maqueta/js/radar.js
@@ -1,31 +1,33 @@
-﻿var randomScalingFactor = function() {
+function createRadar(datos){
+
+console.log("radar chart por aqui");
+
+var randomScalingFactor = function() {
     return Math.round(Math.random() * 100);
-    //return 1;
 };
 
 /////////////////////////////////
-var datos = [
+/*var datos = [
 [1,2,3,4,5],
 [2,3,4,5,6],
 [2,3,8,5,6],
 [2,13,4,5,6]
-];
-var index = 0;
+];*/
+
 
 /////////////////////////////////
-
 
 var color = Chart.helpers.color;
 var config = {
     type: 'radar',
     data: {
-        labels: ['SM1', 'SM4', 'SM5', 'SM6', 'SM'], /*, 'CM1', 'CM3', 'CM5'*/
+        labels: ['SM1', 'SM4', 'SM5', 'SM6', 'SM',"6!"], /*, 'CM1', 'CM3', 'CM5'*/
         datasets: [{
             label: 'Alto Hospicio',
-            backgroundColor: color(window.chartColors.green).alpha(0.2).rgbString(),
-            borderColor: window.chartColors.green,
-            pointBackgroundColor: window.chartColors.green,
-            data:datos[index++]
+            backgroundColor: color(window.chartColors.purple).alpha(0.6).rgbString(),
+            borderColor: window.chartColors.purple,
+            pointBackgroundColor: window.chartColors.purple,
+            data:datos
         }
         /*, {
             label: 'Antofagasta',
@@ -40,9 +42,9 @@ var config = {
                 randomScalingFactor()
             ]
         }*/
-        ]
+      ]
     },
-    options: {
+  /*  options: {
         legend: {
             position: 'top',
         },
@@ -55,37 +57,47 @@ var config = {
                 beginAtZero: true,
                 fontSize: 10,
                 display: true/*Cotas numéricas del radar*/
-            }
+    /*        }
         }
-    }
+    }*/
 };
 
 window.onload = function() {
-    window.myRadar = new Chart(document.getElementById('canvas'), config);
+    window.myRadar = new Chart(document.getElementById('spider'), config);
 };
 
-document.getElementById('randomizeData').addEventListener('click', function() {
-    config.data.datasets.forEach(function(dataset) {
-        /*
-        dataset.data = dataset.data.map(function() {
-            return randomScalingFactor();
-        });
-        */
-        dataset.data = datos[(index++)];
+function actualizar(){
+      config.data.datasets.forEach(function(dataset) {
+        /*  dataset.data = dataset.data.map(function() {
+              return randomScalingFactor();
+          });
+          */
+          dataset.data = datos;
+      });
 
+      window.myRadar.update();
+}
+$(document).ready(
+  function(){
+    document.getElementById('randomizeData').addEventListener('click', function() {
+        config.data.datasets.forEach(function(dataset) {
+          /*  dataset.data = dataset.data.map(function() {
+                return randomScalingFactor();
+            });
+            */
+            dataset.data = datos;
+        });
+
+        window.myRadar.update();
     });
 
-    window.myRadar.update();
-});
-
-
-
-
-
-
-
+}
+)
 
 var colorNames = Object.keys(window.chartColors);
+
+$(document).ready(
+  function(){
 document.getElementById('addDataset').addEventListener('click', function() {
     var colorName = colorNames[config.data.datasets.length % colorNames.length];
     var newColor = window.chartColors[colorName];
@@ -105,18 +117,23 @@ document.getElementById('addDataset').addEventListener('click', function() {
     config.data.datasets.push(newDataset);
     window.myRadar.update();
 });
+}
+)
 
-document.getElementById('addData').addEventListener('click', function() {
-    if (config.data.datasets.length > 0) {
-        config.data.labels.push('dataset #' + config.data.labels.length);
+$(document).ready(
+  function(){
+  document.getElementById('addData').addEventListener('click', function() {
+      if (config.data.datasets.length > 0) {
+          config.data.labels.push('dataset #' + config.data.labels.length);
 
-        config.data.datasets.forEach(function(dataset) {
-            dataset.data.push(randomScalingFactor());
-        });
+          config.data.datasets.forEach(function(dataset) {
+              dataset.data.push(randomScalingFactor());
+          });
 
-        window.myRadar.update();
-    }
-});
+          window.myRadar.update();
+      }
+  });
+
 
 document.getElementById('removeDataset').addEventListener('click', function() {
     config.data.datasets.splice(0, 1);
@@ -132,3 +149,8 @@ document.getElementById('removeData').addEventListener('click', function() {
 
     window.myRadar.update();
 });
+
+}
+)
+
+}//cierre función createRadar
