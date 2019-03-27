50de7e2f6ebee3f60e664dc8ff53a34cdcd92bc7
Marisol Smith <marisolsmith@gmail.com>
2019-03-27 13:48:47 -0300

    fonts 2

--- pagina-maqueta/js/radar.js
+++ pagina-maqueta/js/radar.js
@@ -184,6 +184,7 @@ config = {
             position: 'bottom',
             fullWidth: true,
             labels: {
+                fontFamily: Barlow;
                 fontSize: 13,
                 fullWidth: true,
                 /*padding: 10,*/
@@ -192,6 +193,7 @@ config = {
         title: {
             display: true,
             text: 'Selecciona una Comuna',
+            fontFamily: Barlow;
             fontSize: 18,
             fullWidth: true,
         },
@@ -199,6 +201,7 @@ config = {
             ticks: { /*Cotas numéricas del radar*/
                 beginAtZero: true,
                 max: 100,
+                fontFamily: Barlow;
                 fontSize: 13,
                 display: true
               }
